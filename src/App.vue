8e8434463fe28eb2b95cfaa31dca7ac025209a8f
JOAO DIVINO DA SILVA <60980853+joaodivinod@users.noreply.github.com>
2021-02-21 21:10:55 -0300

    Projeto Reestruturado

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,21 @@
 <template>
 	<div id="app">
-		<usuario-lista :usuarios="usuarios" />
-		<usuario-detalhe />
+    <div class="container">
+      <userList></userList>
+      <userDetails></userDetails>
+    </div>
 	</div>
 </template>
-
 <script>
-import UsuarioLista from './components/UsuarioLista.vue'
-import UsuarioDetalhe from './components/UsuarioDetalhe.vue'
 
-export default {
-	name: "app",
-	components: { UsuarioLista, UsuarioDetalhe },
-    data() {
-        return {
-            usuarios: [
-                { id: 1, nome: 'Ana', idade: 31 },
-                { id: 2, nome: 'Carlos', idade: 22 },
-                { id: 3, nome: 'Daniel', idade: 40 },
-                { id: 4, nome: 'Ema', idade: 17 },
-                { id: 5, nome: 'Lia', idade: 28 }
-            ]
-        }
-    }
-}
 </script>
 
 <style>
-#app {
-	font-family: "Avenir", Helvetica, Arial, sans-serif;
-	-webkit-font-smoothing: antialiased;
-	-moz-osx-font-smoothing: grayscale;
-	text-align: center;
-	color: #2c3e50;
-	margin-top: 60px;
-
-	display: flex;
-	font-size: 2rem;
+body{
+  background-color: #1b1b2f;
 }
-
-#app div {
-	margin: 20px;
+.container{
+  width: 1500px;
+  max-width: 90%;
 }
 </style>
---
+++ src/components/userDetails.vue
@@ -0,0 +1,13 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: "userDetails"
+}
+</script>
+
+<style scoped>
+
+</style>
